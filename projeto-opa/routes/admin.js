93b4d8ee4ca7b7bba1b8d9afa56a94dc7251ee5f
Rafael Alexandrino <contato.rafaelalexandrino@gmail.com>
2022-06-09 20:50:29 -0300

    feature: admin routers

---
+++ projeto-opa/routes/admin.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const productsController = require('../controllers/product');
+
+router.get('/products', productsController.getAllProducts);
+router.get('/products/id/:id', productsController.getProductById);
+router.get('/products/name/:name', productsController.getProductByName);
+router.get('/products/price/:minPrice/:maxPrice', productsController.getProductsByPriceRange);
+
+module.exports = router;
