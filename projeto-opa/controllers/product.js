bf310155267daca2fe421d0139ad65fbf26089f8
Rafael Alexandrino <contato.rafaelalexandrino@gmail.com>
2022-06-09 22:32:01 -0300

    feature: insert and delete operation in database

--- projeto-opa/controllers/product.js
+++ projeto-opa/controllers/product.js
@@ -4,6 +4,15 @@ const productCategory = require('../data/productsCategories');
 const productsDatabasePath = path.join(__dirname, '../data/productsDatabase.json');
 const products = JSON.parse(fs.readFileSync(productsDatabasePath, 'utf-8'));
 
+const productDataParser = (newProductId, productData) => {
+    return ({
+        [newProductId]: {
+            name: productData?.name || null,
+            price: productData?.price || null,
+            category: productData?.category || null,
+        }
+    })
+}
 const productsController = {
     getAllProducts: (req, res) => {
         const context = { products }
@@ -33,7 +42,30 @@ const productsController = {
         } else {
             res.send('please fix the product category name and try again.')
         }
-    }
+    },
+    addProduct: (req, res) => {
+        const productData = req.body;
+        try {
+            const newProductId = Number(Object.keys(products).sort((a, b) => b - a)[0]) + 1;
+            const parsedProductData = productDataParser(newProductId, productData);
+            Object.assign(products, parsedProductData);
+            fs.writeFileSync(productsDatabasePath, JSON.stringify(products))
+            res.send(parsedProductData)
+        } catch (error) {
+            res.send("An error ocurred when trying to add a new product: " + error);
+        }
+    },
+    deleteProduct: (req, res) => {
+        const productId = req.params.id;
+        try {
+            delete products[productId];
+            fs.writeFileSync(productsDatabasePath, JSON.stringify(products))
+            res.send("product deleted with success.")
+        } catch (error) {
+            res.send("An error ocurred when trying to delete a product: " + error);
+        }
+    },
+
 }
 
 module.exports = productsController;
--- projeto-opa/routes/admin.js
+++ projeto-opa/routes/admin.js
@@ -7,5 +7,7 @@ router.get('/products/id/:id', productsController.getProductById);
 router.get('/products/name/:name', productsController.getProductByName);
 router.get('/products/price/:minPrice/:maxPrice', productsController.getProductsByPriceRange);
 router.get('/products/category/:category', productsController.getProductsByCategory);
+router.post('/products/addProduct', productsController.addProduct);
+router.post('/products/deleteProduct/:id', productsController.deleteProduct);
 
 module.exports = router;
