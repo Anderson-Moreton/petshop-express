8eee716779647131bb28f4412c6b2c34b94f020a
Rafael Alexandrino <contato.rafaelalexandrino@gmail.com>
2022-06-09 21:11:17 -0300

    feature: productController getters endpoints

--- projeto-opa/controllers/product.js
+++ projeto-opa/controllers/product.js
@@ -1,8 +1,39 @@
-const productController = {
-    admproduct: (req, res) => {
-        const context = {}
-        res.send('admproduct', context);
+const fs = require('fs');
+const path = require('path');
+const productCategory = require('../data/productsCategories');
+const productsDatabasePath = path.join(__dirname, '../data/productsDatabase.json');
+const products = JSON.parse(fs.readFileSync(productsDatabasePath, 'utf-8'));
+
+const productsController = {
+    getAllProducts: (req, res) => {
+        const context = { products }
+        res.send(context);
+    },
+    getProductById: (req, res) => {
+        const productId = req.params.id;
+        const context = products[productId] || `any product with id ${productId} found`;
+        res.send(context);
+    },
+    getProductByName: (req, res) => {
+        const productName = req.params.name.toLowerCase();
+        const context = Object.values(products).filter(product => product.name?.toLowerCase().includes(productName));
+        res.send(context);
+    },
+    getProductsByPriceRange: (req, res) => {
+        const maxPrice = req.params.maxPrice;
+        const minPrice = req.params.minPrice;
+        const context = Object.values(products).filter(product => product.price < maxPrice && product.price > minPrice);
+        res.send(context);
+    },
+    getProductsByCategory: (req, res) => {
+        const category = req.params.category;
+        if (productCategory[category]) {
+            const context = Object.values(products).filter(product => product.category === category);
+            res.send(context);
+        } else {
+            res.send('please fix the product category name and try again.')
+        }
     }
 }
 
-module.exports = productController;
+module.exports = productsController;
--- projeto-opa/routes/admin.js
+++ projeto-opa/routes/admin.js
@@ -6,5 +6,6 @@ router.get('/products', productsController.getAllProducts);
 router.get('/products/id/:id', productsController.getProductById);
 router.get('/products/name/:name', productsController.getProductByName);
 router.get('/products/price/:minPrice/:maxPrice', productsController.getProductsByPriceRange);
+router.get('/products/category/:category', productsController.getProductsByCategory);
 
 module.exports = router;
