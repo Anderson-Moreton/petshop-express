621cc760375e0d0123f0065b68c097feea961cde
Rafael Alexandrino <97697928+rafaelalexandrino@users.noreply.github.com>
2022-06-27 21:29:26 -0300

    Feat: "cart page frontend and others style improvements" (#8)
    * SPRINT-3 - Entrega (#10)
    
    * paginas de cardapio
    
    * fix: routes brokens fixed
    - Menu
    - Register page
    
    * fix: routes corrections
    
    * refactor: footer and header styling
    
    * refactor: shared.css file refactored
    
    * fix/refactor: food menu pages
    
    * feature: aboutUs page frontend
    
    * refactor: multiple pages styling
    
    * feature: productOffer component partial
    
    * Feat: "CRUD implementation" (#9)
    
    * feature: admin routers
    
    * feature: productController getters endpoints
    
    * feature: productCategory types
    
    * feature: productsDatabase.json
    
    * feature: productsDatabase with more entries
    
    * feature: productsDatabase with more entries
    
    * feature: insert and delete operation in database
    
    Co-authored-by: Ruan Calixto <ruancalixto38@gmail.com>
    
    * Feat: "Database creation with Product and Categories models" (#11)
    
    * feature: database initial eer diagram
    
    * feature: sequelize configuration and dotenv pack
    
    * feature: Product model created
    
    * feature: Category model created
    
    * Feat: "Add product backend and frontend flow" (#12)
    
    * feature: productsController addProduct db conn
    
    * feature: addProduct controller
    
    * feature: addProduct route
    
    * feature: addProduct form view
    
    * feature: deleteProduct view
    
    * feature: CRUD operations routes and controllers
    
    Co-authored-by: Anderson Moreton <anderson.moreton@gmail.com>
    Co-authored-by: Ruan Calixto <ruancalixto38@gmail.com>

--- projeto-opa/controllers/product.js
+++ projeto-opa/controllers/product.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 const path = require('path');
+const { Op } = require("sequelize");
 const productCategory = require('../data/productsCategories');
 const productsDatabasePath = path.join(__dirname, '../data/productsDatabase.json');
-const products = JSON.parse(fs.readFileSync(productsDatabasePath, 'utf-8'));
+const db = require('../models')
+const productCategory = require('../data/productsCategories');
+const productsDatabasePath = path.join(__dirname, '../data/productsDatabase.json');
 
 const productDataParser = (newProductId, productData) => {
     return ({
@@ -13,24 +16,117 @@ const productDataParser = (newProductId, productData) => {
         }
     })
 }
+
 const productsController = {
-    getAllProducts: (req, res) => {
-        const context = { products }
+    getAllProducts: async (req, res) => {
+        const allProducts = await db.Product.findAll()
+        const context = { allProducts }
         res.send(context);
     },
-    getProductById: (req, res) => {
+    getProductById: async (req, res) => {
         const productId = req.params.id;
-        const context = products[productId] || `any product with id ${productId} found`;
-        res.send(context);
+        db.Product.findByPk(productId).then((result) => {
+            if (result) {
+                res.send(result);
+            } else {
+                res.send(`any product with id ${productId} found`)
+            }
+        }).catch((error) => {
+            res.send(`any product with id ${productId} found`)
+        })
     },
     getProductByName: (req, res) => {
         const productName = req.params.name.toLowerCase();
-        const context = Object.values(products).filter(product => product.name?.toLowerCase().includes(productName));
-        res.send(context);
+        db.Product.findAll({
+            where: {
+                name: {
+                    [Op.substring]: productName
+                }
+            }
+        }).then((result) => {
+            if (result) {
+                res.send(result);
+            } else {
+                res.send(`any product with name ${productName} found`)
+            }
+        }).catch((error) => {
+            res.send(`any product with name ${productName} found`)
+        })
     },
     getProductsByPriceRange: (req, res) => {
         const maxPrice = req.params.maxPrice;
         const minPrice = req.params.minPrice;
+        db.Product.findAll({
+            where: {
+                price: {
+                    [Op.between]: [minPrice, maxPrice]
+                }
+            }
+        }).then((result) => {
+            if (result) {
+                res.send(result);
+            } else {
+                res.send(`any product with price between ${minPrice} and ${maxPrice} found`)
+            }
+        }).catch((error) => {
+            res.send(`any product with price between ${minPrice} and ${maxPrice} found`)
+        })
+    },
+    getProductsByCategory: (req, res) => {
+        const category = req.params.category;
+        db.Product.findAll({
+            where: {
+                category: {
+                    [Op.eq]: category
+                }
+            }
+        }).then((result) => {
+            if (result) {
+                res.send(result);
+            } else {
+                res.send('please fix the product category name and try again.')
+            }
+        }).catch((error) => {
+            res.send('please fix the product category name and try again.')
+        })
+    },
+    addProduct: (req, res) => {
+        const productData = req.body;
+        const parsedProductData = {
+            ...productData,
+            category: parseInt(productData.category),
+            price: parseFloat(productData.price.replace(',', '.'))
+        }
+
+        db.Product.create(parsedProductData).then(() => {
+            res.render('admin/addProduct');
+        }).catch((error) => {
+            res.send("An error ocurred when trying to add a new product: " + error);
+        })
+    },
+    addProductView: (req, res) => {
+        res.render('admin/addProduct')
+    },
+    deleteProduct: (req, res) => {
+        const productId = req.body.productId;
+        db.Product.destroy({
+            where: {
+                id: {
+                    [Op.eq]: productId
+                }
+            }
+        }).then((result) => {
+            if (result) {
+                res.render('admin/deleteProduct');
+            } else {
+                res.send('An error ocurred when trying to delete a product')
+            }
+        }).catch((error) => {
+            res.send("An error ocurred when trying to delete a product: " + error);
+        })
+    },
+    deleteProductView: (req, res) => {
+        res.render('admin/deleteProduct')
         const context = Object.values(products).filter(product => product.price < maxPrice && product.price > minPrice);
         res.send(context);
     },
