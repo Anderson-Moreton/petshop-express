64ff562c010f18610f874a40f770c51e5e3ca99c
Ruan Calixto <ruancalixto38@gmail.com>
2022-08-22 19:59:56 -0300

    inicio

--- projeto-opa/controllers/main.js
+++ projeto-opa/controllers/main.js
@@ -1,7 +1,38 @@
+const productsController = require("./product");
+
 const generalController = {
+    
     home: (req, res) => {
-        const context = {}
-        res.render('index', context);
+        // const produtos = [
+        //     {
+        //       id: 10,
+        //       nome: 'X-tudo',
+        //       valor: 16.99,
+        //       foto: '/img/burguer01.jpg'
+        //     },
+        //     {
+        //       id: 12,
+        //       nome: 'x-especial',
+        //       valor: 20,
+        //       foto: '/img/burguer02.jpg'
+        //     },
+        //     {
+        //       id: 17,
+        //       nome: 'x-gulosso',
+        //       valor: 235.99,
+        //       foto: '/img/burguer03.jpg'
+        //     },
+        //     {
+        //         id: 13,
+        //         nome: 'x-max',
+        //         valor: 235.99,
+        //         foto: '/img/burguer04.jpg'
+        //       },
+        //   ];
+        const produtos = productsController.getAllProducts()
+          
+        
+        res.render('index',{produtos});
     }
 }
 
