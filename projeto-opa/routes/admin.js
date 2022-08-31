3d244fd44ab965ab30de6823431c25a83b03329f
Ruan Calixto <ruancalixto38@gmail.com>
2022-08-31 16:27:15 -0300

    limpando codico

--- projeto-opa/routes/admin.js
+++ projeto-opa/routes/admin.js
@@ -4,7 +4,7 @@ const productsController = require('../controllers/product');
 const admController = require('../controllers/adm') /*Anderson 30/08*/
 
 
-router.get('/admin', admController); /*Anderson 30/08*/
+
 
 router.get('/products', productsController.getAllProducts);
 router.get('/products/id/:id', productsController.getProductById);
